a7ef118b78e43080de11174bc099889b6d7dfb24
Gunawan10 <gupuspo@gmail.com>
2016-03-06 10:15:43 +0700

    add row table javascript fix

--- application/views/konten/cucian/tambah.php
+++ application/views/konten/cucian/tambah.php
@@ -15,13 +15,13 @@
     <div class="grid">
         <div class="row cells2">
             <div class="cell">
-                <h5>Nama Pelanggan</h5>
+                <label>Nama Pelanggan</label>
                 <div class="input-control text full-size">
                     <input type="text" name="nama_pelanggan" placeholder="Nama lengkap">
                 </div>
             </div>
             <div class="cell">
-                <h5>No telepon</h5>
+                <label>No telepon</label>
                 <div class="input-control text full-size">
                     <input type="text" name="no_telepon" placeholder="Nama lengkap">
                 </div>
@@ -29,7 +29,7 @@
         </div>
         <div class="row cells2">
             <div class="cell">
-                <h5>Metode Pengiriman</h5>
+                <label>Metode Pengiriman</label>
                 <select id="select" name="kode_layanan" class="js-select full-size">
                     <?php
                     foreach ($kirim as $data) { 
@@ -39,7 +39,7 @@
                 </select>
             </div>
             <div class="cell">
-                <h5>Paket Kerja</h5>
+                <label>Paket Kerja</label>
                 <select id="select2" name="kode_layanan" class="js-select full-size">
                     <?php
                     foreach ($paket as $data) { 
@@ -58,12 +58,15 @@
             </div>
         </div>
     </div>
-    <table class="table striped border bordered">
-    <thead>
+    <label>List Item</label><br><br>
+    <button type="button" class="button primary small-button" onclick="addRow('tablelist');"><span class="mif-plus"></span> Tambah Item</button>
+    <button type="button" class="button danger small-button" onclick="deleteRow('tablelist');"><span class="mif-bin"></span> Hapus Item</button>
+    <table id="tablelist" class="table border bordered">
+        <thead>
         <tr>
             <td>
                 <label class="input-control checkbox small-check no-margin">
-                        <input type="checkbox" name="check[]" value="0">
+                        <input type="checkbox" onclick="for(c in document.getElementsByName('check[]')) document.getElementsByName('check[]').item(c).checked = this.checked">
                         <span class="check"></span>
                 </label>
             </td>
@@ -74,18 +77,13 @@
             <td>Ukuran Barang</td>
             <td>Qty</td>
         </tr>
-    </thead>
-    <tbody>
+        </thead>
+        <tbody>
         <tr>
+            <td><input type="checkbox" name="check[]" value="1"></td>
             <td>
-                <label class="input-control checkbox small-check no-margin">
-                        <input type="checkbox" name="check[]" value="0">
-                        <span class="check"></span>
-                </label>
-            </td>
-            <td>
-            <div class="input-control full-size" data-role="select">
-                <select name="kode_layanan">
+            <div class="input-control select full-size">
+                <select name="kode_layanan[]">
                     <?php
                     foreach ($layanan as $data) { 
                     ?>
@@ -95,8 +93,8 @@
             </div>
             </td>
             <td>
-            <div class="input-control full-size" data-role="select">
-                <select name="kode_jenis">
+            <div class="input-control select full-size">
+                <select name="kode_jenis[]">
                     <?php
                     foreach ($jeniscucian as $data) { 
                     ?>
@@ -106,8 +104,8 @@
             </div>
             </td>
             <td>
-            <div class="input-control full-size" data-role="select">
-                <select name="kode_kategori">
+            <div class="input-control select full-size">
+                <select name="kode_kategori[]">
                     <?php
                     foreach ($kategori as $data) { 
                     ?>
@@ -117,8 +115,8 @@
             </div>
             </td>
             <td>
-            <div class="input-control full-size" data-role="select">
-                <select name="kode_barang">
+            <div class="input-control select full-size">
+                <select name="kode_barang[]">
                     <?php
                     foreach ($barang as $data) { 
                     ?>
@@ -128,8 +126,8 @@
             </div>
             </td>
             <td>
-            <div class="input-control full-size" data-role="select">
-                <select name="kode_ukuran">
+            <div class="input-control select full-size">
+                <select name="kode_ukuran[]">
                     <?php
                     foreach ($ukuran as $data) { 
                     ?>
@@ -140,7 +138,7 @@
             </td>
             <td style="width: 100px">
                 <div class="input-control text full-size">
-                    <input type="text" name="qty" placeholder="qty">
+                    <input type="text" name="qty[]" placeholder="qty">
                 </div>
             </td>
         </tr>
@@ -148,3 +146,62 @@
     </table>
     </form>
 </div>
+<script language="javascript">
+        function addRow(tableID) {
+          var table = document.getElementById(tableID);
+ 
+            var rowCount = table.rows.length;
+            var row = table.insertRow(rowCount);
+ 
+            var colCount = table.rows[1].cells.length;
+ 
+            for(var i=0; i<colCount; i++) {
+ 
+                var newcell = row.insertCell(i);
+ 
+                newcell.innerHTML = table.rows[1].cells[i].innerHTML;
+                //alert(newcell.childNodes);
+                switch(newcell.childNodes[0].type) {
+                    case "text":
+                            newcell.childNodes[0].value = "";
+                            break;
+                    case "checkbox":
+                            newcell.childNodes[0].checked = false;
+                            newcell.childNodes[0].value = rowCount;
+                            break;
+                    case "select-one":
+                            newcell.childNodes[0].selectedIndex = 0;
+                            break;
+                }
+            }
+
+
+        }
+ 
+        function deleteRow(tableID) {
+            try {
+            var table = document.getElementById(tableID);
+            var rowCount = table.rows.length;
+ 
+            for(var i=0; i<rowCount; i++) {
+                var row = table.rows[i];
+                var chkbox = row.cells[0].childNodes[0];
+                row.cells[0].childNodes[0].value = i+1;
+                if(null != chkbox && true == chkbox.checked) {
+                    if(rowCount <= 1) {
+                        alert("Cannot delete all the rows.");
+                        break;
+                    }
+                    table.deleteRow(i);
+                    rowCount--;
+                    i--;
+                }
+ 
+ 
+            }
+            }catch(e) {
+                alert(e);
+            }
+        }
+ 
+    </script>
