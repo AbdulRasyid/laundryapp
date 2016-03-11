2db98bd1ba393f1c5f19beb0e24009d97fe9bc17
Gunawan10 <gupuspo@gmail.com>
2016-03-11 11:06:44 +0700

    database update

--- laundry(11 maret 2016).sql
+++ laundry(11 maret 2016).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 06, 2016 at 08:18 
+-- Generation Time: Mar 11, 2016 at 05:04 
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -114,7 +114,7 @@ CREATE TABLE `list_cucian` (
   `kode_barang` varchar(3) NOT NULL,
   `kode_layanan` varchar(3) NOT NULL,
   `kode_jenis` varchar(3) NOT NULL,
-  `kode_ukuran` varchar(3) NOT NULL,
+  `kode_ukuran` varchar(5) NOT NULL,
   `qty` int(11) NOT NULL,
   `harga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -124,7 +124,8 @@ CREATE TABLE `list_cucian` (
 --
 
 INSERT INTO `list_cucian` (`id`, `kode_resi`, `kode_barang`, `kode_layanan`, `kode_jenis`, `kode_ukuran`, `qty`, `harga`) VALUES
-(4, '264262', 'ALL', 'CBS', 'KLN', 'ALL', 2, 14000);
+(30, '228864656', 'ALL', 'CBS', 'KLN', 'ALL', 1, 7000),
+(31, '228864656', 'ALL', 'CKS', 'KLN', 'ALL', 2, 10000);
 
 -- --------------------------------------------------------
 
@@ -160,8 +161,8 @@ CREATE TABLE `pelanggan` (
   `alamat` text NOT NULL,
   `kode_resi` varchar(20) NOT NULL,
   `status` varchar(10) NOT NULL,
-  `tanggal_daftar` datetime NOT NULL,
-  `tanggal_ambil` datetime DEFAULT NULL,
+  `tanggal_daftar` varchar(50) DEFAULT NULL,
+  `tanggal_ambil` varchar(50) DEFAULT NULL,
   `kode_pengiriman` varchar(3) NOT NULL,
   `kode_paket` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -171,7 +172,7 @@ CREATE TABLE `pelanggan` (
 --
 
 INSERT INTO `pelanggan` (`id`, `nama_pelanggan`, `no_telepon`, `alamat`, `kode_resi`, `status`, `tanggal_daftar`, `tanggal_ambil`, `kode_pengiriman`, `kode_paket`) VALUES
-(4, 'Abdul Zakir', '12312323', 'kfdsjfkdjfd', '264262', 'pending', '2016-03-06 07:20:27', NULL, 'AJT', 'ISW');
+(8, 'Abimanyu', '089671907680', 'Jalan condet batu ampar', '228864656', 'PND', '03/10/2016 23:32:38', '03/11/2016 10:26:11', 'AJT', 'RGR');
 
 -- --------------------------------------------------------
 
@@ -182,9 +183,10 @@ INSERT INTO `pelanggan` (`id`, `nama_pelanggan`, `no_telepon`, `alamat`, `kode_r
 CREATE TABLE `pembayaran` (
   `id` int(11) NOT NULL,
   `kode_resi` varchar(20) NOT NULL,
+  `nama_pelanggan` varchar(50) NOT NULL,
   `harga_total` int(11) NOT NULL,
   `user_bayar` int(11) NOT NULL,
-  `tanggal_bayar` datetime DEFAULT NULL,
+  `tanggal_bayar` varchar(50) DEFAULT NULL,
   `uang_kembali` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -192,8 +194,8 @@ CREATE TABLE `pembayaran` (
 -- Dumping data for table `pembayaran`
 --
 
-INSERT INTO `pembayaran` (`id`, `kode_resi`, `harga_total`, `user_bayar`, `tanggal_bayar`, `uang_kembali`) VALUES
-(1, '264262', 21000, 0, NULL, 0);
+INSERT INTO `pembayaran` (`id`, `kode_resi`, `nama_pelanggan`, `harga_total`, `user_bayar`, `tanggal_bayar`, `uang_kembali`) VALUES
+(8, '228864656', 'Abimanyu', 21000, 25000, '03/11/2016 10:26:11', 4000);
 
 -- --------------------------------------------------------
 
@@ -219,6 +221,28 @@ INSERT INTO `pengiriman` (`kode_pengiriman`, `nama_pengiriman`, `harga_kirim`) V
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `perusahaan`
+--
+
+CREATE TABLE `perusahaan` (
+  `id` int(11) NOT NULL,
+  `namaperusahaan` varchar(15) NOT NULL,
+  `notelepon` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `deskripsi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `perusahaan`
+--
+
+INSERT INTO `perusahaan` (`id`, `namaperusahaan`, `notelepon`, `email`, `alamat`, `deskripsi`) VALUES
+(1, 'MM - Laundry', '021998722', 'laundryapp@gmail.com', 'jalan panca warga no 2132132', 'adfdasf');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `rakit_harga`
 --
 
@@ -239,6 +263,27 @@ INSERT INTO `rakit_harga` (`kode_rakit`, `kode_layanan`, `kode_jenis`, `kode_bar
 ('CBS-001', 'CBS', 'KLN', 'ALL', 'ALL', '7000'),
 ('CBS-002', 'CBS', 'STN', 'MKM', 'BSR', '6000'),
 ('CKS-001', 'CKS', 'KLN', 'ALL', 'ALL', '5000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status_data`
+--
+
+CREATE TABLE `status_data` (
+  `kode_status` varchar(3) NOT NULL,
+  `nama_status` varchar(20) NOT NULL,
+  `default_input` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `status_data`
+--
+
+INSERT INTO `status_data` (`kode_status`, `nama_status`, `default_input`) VALUES
+('PND', 'Pending', 'yes'),
+('PRS', 'Proses', 'no'),
+('SLS', 'Selesai', 'no');
 
 -- --------------------------------------------------------
 
@@ -290,16 +335,21 @@ CREATE TABLE `user` (
   `namalengkap` varchar(50) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(50) NOT NULL,
+  `password_word` varchar(50) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `tentang` text
+  `tentang` text,
+  `status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `namalengkap`, `username`, `password`, `email`, `tentang`) VALUES
-(1, 'Administrator', 'admin', '0192023a7bbd73250516f069df18b500', 'admin@webmaster.com', 'Developer cool');
+INSERT INTO `user` (`id`, `namalengkap`, `username`, `password`, `password_word`, `email`, `tentang`, `status`) VALUES
+(1, 'Administrator', 'admin', '0192023a7bbd73250516f069df18b500', 'admin123', 'admin@webmaster.com', 'Developer cool', 'admin'),
+(2, 'Operator', 'operator1', '2407bd807d6ca01d1bcd766c730cec9a', 'operator123', '', '', 'OP'),
+(3, 'Kasir', 'kasir1', 'de28f8f7998f23ab4194b51a6029416f', 'kasir123', '', '', 'KS'),
+(4, 'Pencuci', 'pencuci1', '3037287e74b0cf365f6782d3d8be99d3', 'pencuci123', '', '', 'PC');
 
 --
 -- Indexes for dumped tables
@@ -335,7 +385,11 @@ ALTER TABLE `layanan`
 -- Indexes for table `list_cucian`
 --
 ALTER TABLE `list_cucian`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kode_barang` (`kode_barang`),
+  ADD KEY `kode_layanan` (`kode_layanan`),
+  ADD KEY `kode_jenis` (`kode_jenis`),
+  ADD KEY `kode_ukuran` (`kode_ukuran`);
 
 --
 -- Indexes for table `paket_kerja`
@@ -347,7 +401,10 @@ ALTER TABLE `paket_kerja`
 -- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `status` (`status`),
+  ADD KEY `kode_pengiriman` (`kode_pengiriman`),
+  ADD KEY `kode_paket` (`kode_paket`);
 
 --
 -- Indexes for table `pembayaran`
@@ -362,6 +419,12 @@ ALTER TABLE `pengiriman`
   ADD PRIMARY KEY (`kode_pengiriman`);
 
 --
+-- Indexes for table `perusahaan`
+--
+ALTER TABLE `perusahaan`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `rakit_harga`
 --
 ALTER TABLE `rakit_harga`
@@ -370,6 +433,12 @@ ALTER TABLE `rakit_harga`
   ADD KEY `kode_jenis` (`kode_jenis`),
   ADD KEY `kode_barang` (`kode_barang`),
   ADD KEY `kode_ukuran` (`kode_ukuran`);
+
+--
+-- Indexes for table `status_data`
+--
+ALTER TABLE `status_data`
+  ADD PRIMARY KEY (`kode_status`);
 
 --
 -- Indexes for table `ukuran`
@@ -398,22 +467,27 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `list_cucian`
 --
 ALTER TABLE `list_cucian`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `perusahaan`
+--
+ALTER TABLE `perusahaan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
@@ -429,6 +503,23 @@ ALTER TABLE `barang`
 --
 ALTER TABLE `jenis_cucian`
   ADD CONSTRAINT `jenis_cucian_ibfk_1` FOREIGN KEY (`kode_ukuran`) REFERENCES `ukuran` (`kode_ukuran`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `list_cucian`
+--
+ALTER TABLE `list_cucian`
+  ADD CONSTRAINT `list_cucian_ibfk_1` FOREIGN KEY (`kode_ukuran`) REFERENCES `ukuran_benda` (`kode_ukuran`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `list_cucian_ibfk_2` FOREIGN KEY (`kode_jenis`) REFERENCES `jenis_cucian` (`kode_jenis`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `list_cucian_ibfk_3` FOREIGN KEY (`kode_layanan`) REFERENCES `layanan` (`kode_layanan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `list_cucian_ibfk_4` FOREIGN KEY (`kode_barang`) REFERENCES `barang` (`kode_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD CONSTRAINT `pelanggan_ibfk_1` FOREIGN KEY (`status`) REFERENCES `status_data` (`kode_status`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pelanggan_ibfk_2` FOREIGN KEY (`kode_pengiriman`) REFERENCES `pengiriman` (`kode_pengiriman`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pelanggan_ibfk_3` FOREIGN KEY (`kode_paket`) REFERENCES `paket_kerja` (`kode_paket`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `rakit_harga`
